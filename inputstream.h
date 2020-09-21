a940a5335c8f10146497aacfa3eaa50c2041aaeb
alex <chudinov@inbox.ru>
2020-09-21 14:37:30 +0300

    Full vesrsion before test

--- inputstream.h
+++ inputstream.h
@@ -1,6 +1,9 @@
 ﻿#ifndef INPUTSTREAM_H
 #define INPUTSTREAM_H
 
+#include <vector>
+#include <algorithm>
+
 #include "InflowBoundaryModel.H"
 #include "polyMesh.H"
 #include "constants.H"
@@ -35,7 +38,16 @@ class InputStream
 
     scalarList componentMasses_;
 
+    labelList componentTypeId_;
+
     void checkTemperature() const;
+
+    vector genMoleculeVelocity(
+            Random& rndGen,
+            const vector& n,
+            const vector& U,
+            double mass,
+            double T);
 
 public:
 
@@ -81,6 +93,30 @@ void InputStream<CloudType>::checkTemperature() const
 }
 
 template<class CloudType>
+vector InputStream<CloudType>::genMoleculeVelocity(
+        Random &rndGen,
+        const vector &n,
+        const vector &U,
+        double mass,
+        double T)
+{
+    CloudType& cloud = this->owner();
+    scalar s = cloud.maxwellianMostProbableSpeed(T, mass) / sqrt(2.0);
+    scalar vInf = (n & U) > 0.0 ? std::sqrt(std::pow((n & U), 2) + 9.0 * s * s)
+                                : 3.0 * s;
+    vector moleculeVelocity;
+    do
+    {
+        moleculeVelocity = s *
+                vector(rndGen.scalarNormal(), rndGen.scalarNormal(), rndGen.scalarNormal())
+                + U;
+    }
+    while((moleculeVelocity & n) / vInf < rndGen.scalar01());
+
+    return moleculeVelocity;
+}
+
+template<class CloudType>
 InputStream<CloudType>::InputStream(const dictionary &dict, CloudType &cloud)
     :
       InflowBoundaryModel<CloudType>(dict, cloud, typeName)
@@ -100,6 +136,7 @@ InputStream<CloudType>::InputStream(const dictionary &dict, CloudType &cloud)
     componentNames_ = componentAbundances.toc();
     componentAbundances_.setSize(componentNames_.size());
     componentMasses_.setSize(componentNames_.size());
+    componentTypeId_.setSize(componentNames_.size());
     const dictionary& moleculeProperties = cloud.particleProperties().subDict(
                 moleculePropertiesDicrName_);
     Info << "Create custom properties of molecules for molecular inflow: " << nl;
@@ -108,8 +145,9 @@ InputStream<CloudType>::InputStream(const dictionary &dict, CloudType &cloud)
         componentAbundances_[n] = componentAbundances.lookup<scalar>(componentNames_[n]);
         componentMasses_[n] = moleculeProperties.subDict(componentNames_[n])
                 .lookup<scalar>(massDictKey_);
-
+        componentTypeId_[n] = findIndex(cloud.typeIdList(), componentNames_[n]);
         Info << componentNames_[n] << " : "
+             << componentTypeId_[n] << " : "
              << componentAbundances_[n] << " : "
              << componentMasses_[n] << nl;
     }
@@ -126,6 +164,9 @@ InputStream<CloudType>::~InputStream()
 template<class CloudType>
 void InputStream<CloudType>::inflow()
 {
+    //Cumulative fractions to simulate random tet at face
+    static std::vector<scalar> faceTetsFractions;
+
     CloudType& cloud = this->owner();
 
     const polyMesh & mesh = cloud.mesh();
@@ -149,13 +190,6 @@ void InputStream<CloudType>::inflow()
 
             const polyPatch& patch = mesh.boundaryMesh()[patchi];
 
-            scalarField mostProbableSpeed = cloud.maxwellianMostProbableSpeed(
-                        boundaryT[patchi],
-                        componentMasses_[componenti]);
-
-            scalarField sCosTheta = (boundaryU[patchi] & -patch.faceAreas() / patch.magFaceAreas())
-                    / mostProbableSpeed;
-
             forAll(patch, facei){
                 const face& f = patch[facei];
 
@@ -165,16 +199,63 @@ void InputStream<CloudType>::inflow()
 
                 const vector& fC = patch.faceCentres()[facei];
 
-                scalar fA = patch.magFaceAreas()[facei];
+                const scalar fA = patch.magFaceAreas()[facei];
+
+                const scalar mostProbableSpeed = cloud.maxwellianMostProbableSpeed(
+                        boundaryT[patchi][facei],
+                        componentMasses_[componenti]);
+
+                const scalar sCosTheta = (boundaryU[patchi][facei] & -patch.faceAreas()[facei])
+                        / patch.magFaceAreas()[facei] / mostProbableSpeed;
 
                 List<tetIndices> faceTets = polyMeshTetDecomposition::faceTetIndices
-                (
-                    mesh,
-                    globalFaceIndex,
-                    celli
-                );
+                        (
+                            mesh,
+                            globalFaceIndex,
+                            celli
+                            );
 
+                faceTetsFractions.assign(faceTets.size(), 0.0);
+                scalar cumulativeSum = 0.0;
+                forAll(faceTets, faceTeti){
+                    cumulativeSum += faceTets[faceTeti].faceTri(mesh).mag() / fA;
+                    faceTetsFractions[faceTeti] = cumulativeSum;
+                }
 
+                scalar nP = fA * rhoN[patchi][facei] * deltaT / cloud.nParticle()
+                        * mostProbableSpeed
+                        *(exp(-sqr(sCosTheta)) + sqrtPi*sCosTheta*(1 + erf(sCosTheta)))
+                        /(2.0*sqrtPi);
+
+                label numberOfParticles = rndGen.scalar01() > .5 ?
+                            label(nP) : label(nP) + 1;
+
+                for(label i = 0; i < numberOfParticles; ++i){
+                    label triIdx = std::distance(faceTetsFractions.begin(),
+                                                 std::lower_bound(
+                                                     faceTetsFractions.begin(),
+                                                     faceTetsFractions.end(), rndGen.scalar01()));
+                    triIdx = triIdx == faceTets.size() ? triIdx - 1 : triIdx;
+
+                    const point p = faceTets[triIdx].faceTri(mesh).randomPoint(rndGen);
+
+                    const vector molecularVecolity = genMoleculeVelocity(
+                                rndGen,
+                                -patch.faceAreas()[facei],
+                                boundaryU[patchi][facei],
+                                componentMasses_[componenti],
+                                boundaryT[patchi][facei]);
+
+                    scalar Ei = cloud.equipartitionInternalEnergy
+                    (
+                        boundaryT[patchi][facei],
+                        cloud.constProps(componentTypeId_[componenti]).internalDegreesOfFreedom()
+                    );
+
+                    cloud.addNewParcel(p, celli, molecularVecolity, Ei, componentTypeId_[componenti]);
+
+                    particlesInserted++;
+                }
             }
         }
     }
